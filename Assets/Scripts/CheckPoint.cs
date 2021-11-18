2a99e6a9598e2f5e4c8cf79b30ddb32dedda05d2
ttmn223 <xuanrendi089@gmail.com>
2021-11-18 15:16:13 +0900

    タイマー修正

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -5,8 +5,19 @@ using UnityEngine.UI;
 
 public class CheckPoint : MonoBehaviour
 {
-    private int Checkcount;
-    public GameObject player;
+    //通ったチェックポイント回数
+    private int Checkcount_player;
+    private int Checkcount_ai;
+    public GameObject Player;
+    public GameObject AI;
+    public Text cleartime;
+    //通過したチェックポイントを入れる
+    public int LastCheckPoint;
+
+    //　player用テキスト
+    public Text player;
+    //　AIテキスト
+    public Text ai;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,20 +27,43 @@ public class CheckPoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Checkcount_ai != Checkcount_player)
+        {
+            if((Checkcount_player - Checkcount_ai) >= 2)
+            {
+                //playerの勝ち
+                Debug.Log("プレイヤーの勝ち");
+                //cleartime = timerText;
+            }
+            else if ((Checkcount_player - Checkcount_ai) <= -2)
+            {
+                //aiの勝ち
+                Debug.Log("AIの勝ち");
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "CheckPoint")//チェックポイントに触れた
         {
-            Destroy(other.gameObject);
-            Checkcount += 1;
-            Debug.Log("Check" + Checkcount);
+            if (gameObject.tag == "AI")
+            {
+                Checkcount_ai += 1;
+                Debug.Log("Check_a" + Checkcount_ai);
+                ai.text = gameObject.name;
+            }
+
+            if (other.gameObject.tag == "Player")//チェックポイントに触れた
+            {
+                Checkcount_player += 1;
+                Debug.Log("Check_p" + Checkcount_player);
+                player.text = gameObject.name;
+            }
         }
         if (other.gameObject.tag == "Start")//スタートラインに触れた
         {
-            if (Checkcount == 18)//チェックポイントをすべて通ったか
+            if (Checkcount_player == 18)//チェックポイントをすべて通ったか
             {
                 Debug.Log("GOAL!");
             }
