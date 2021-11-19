0b3af1c5e09b51f27242d4a52c9384d60f334ef7
ttmn223 <xuanrendi089@gmail.com>
2021-11-19 09:25:00 +0900

    test

--- Assets/Scripts/CheckPoint.cs
+++ Assets/Scripts/CheckPoint.cs
@@ -8,47 +8,20 @@ public class CheckPoint : MonoBehaviour
     //通ったチェックポイント回数
     private int Checkcount_player;
     private int Checkcount_ai;
+    public GameObject Player;
+    public GameObject AI;
     public Text cleartime;
     //通過したチェックポイントを入れる
-    public string LastCheckPoint;
-    int LastCheckPointa =0;
-    int LastCheckPointb =0;
+    public int LastCheckPoint;
 
     //　player用テキスト
     public Text player;
     //　AIテキスト
     public Text ai;
     // Start is called before the first frame update
-
-    private void OnTriggerEnter(Collider other)
-    {
-        //Debug.Log(other.gameObject.name);
-        if (other.gameObject.tag == "Player")//チェックポイントに触れた
-        {
-            Debug.Log("Trigger");
-            //LastCheckPointa =LastCheckPointb +1;//LastCheckPointa < LastCheckPointb || (LastCheckPointa - LastCheckPointb) == 18
-            //逆走禁止設定
-            if (LastCheckPoint != gameObject.name)
-            {
-                Checkcount_player += 1;
-                Debug.Log("Check_p:" + Checkcount_player);
-                player.text = "Player:" + gameObject.name;
-                LastCheckPoint = gameObject.name;
-            }
-        }
-        else if (other.gameObject.tag == "AI")//チェックポイントに触れた
-        {
-            Checkcount_ai += 1;
-            Debug.Log("Check_a:" + Checkcount_ai);
-            ai.text = "AI:" + gameObject.name;
-        }
-    }
     void Start()
     {
-        Checkcount_player = 0;
-        Checkcount_ai = 0;
-        LastCheckPointa = 0;
-        LastCheckPointb = 0;
+
     }
 
     // Update is called once per frame
@@ -56,16 +29,47 @@ public class CheckPoint : MonoBehaviour
     {
         if (Checkcount_ai != Checkcount_player)
         {
-            if ((Checkcount_player - Checkcount_ai) >= 3)
+            if((Checkcount_player - Checkcount_ai) >= 2)
             {
                 //playerの勝ち
                 Debug.Log("プレイヤーの勝ち");
                 //cleartime = timerText;
             }
-            else if ((Checkcount_player - Checkcount_ai) <= -3)
+            else if ((Checkcount_player - Checkcount_ai) <= -2)
             {
                 //aiの勝ち
                 Debug.Log("AIの勝ち");
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CheckPoint")//チェックポイントに触れた
+        {
+            if (gameObject.tag == "AI")
+            {
+                Checkcount_ai += 1;
+                Debug.Log("Check_a" + Checkcount_ai);
+                ai.text = gameObject.name;
+            }
+
+            if (other.gameObject.tag == "Player")//チェックポイントに触れた
+            {
+                Checkcount_player += 1;
+                Debug.Log("Check_p" + Checkcount_player);
+                player.text = gameObject.name;
+            }
+        }
+        if (other.gameObject.tag == "Start")//スタートラインに触れた
+        {
+            if (Checkcount_player == 18)//チェックポイントをすべて通ったか
+            {
+                Debug.Log("GOAL!");
+            }
+            else
+            {
+                Debug.Log("NO GOAL!");
             }
         }
     }
