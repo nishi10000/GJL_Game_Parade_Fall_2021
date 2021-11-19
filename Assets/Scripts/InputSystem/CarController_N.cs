715551b3311c189caf0f254b4f3b5f98ac30998d
yu <34505055+nishi10000@users.noreply.github.com>
2021-11-19 16:57:17 +0900

    スマホ用のタッチシーンの追加

--- Assets/Scripts/InputSystem/CarController_N.cs
+++ Assets/Scripts/InputSystem/CarController_N.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityStandardAssets.CrossPlatformInput;
 using UnityStandardAssets.Vehicles.Car;
 
@@ -10,12 +11,58 @@ public class CarController_N : MonoBehaviour
     [SerializeField]
     InputSystemMove InputSystemMove;
 
+    /*
+    [SerializeField]
+    Text Debug_text1;
+    [SerializeField]
+    Text Debug_text2;
+    */
+    private float h;
+    private float v;
+
     private void Awake()
     {
         // get the car controller
         m_Car = GetComponent<CarController>();
     }
 
+    public void OnAButton()
+    {
+        h = -1.0f;
+
+    }
+    public void UpAButton()
+    {
+        h = 0.0f;
+
+    }
+    public void OnDButton()
+    {
+        h = 1.0f;
+    }
+    public void UpDButton()
+    {
+        h = 0.0f;
+    }
+    public void OnSButton()
+    {
+        v = -1.0f;
+
+    }
+    public void UpSButton()
+    {
+        v = 0.0f;
+
+    }
+
+    public void OnWButton()
+    {
+        v = 1.0f;
+    }
+    public void UpWButton()
+    {
+        v = 0.0f;
+    }
 
     private void FixedUpdate()
     {
@@ -23,9 +70,12 @@ public class CarController_N : MonoBehaviour
         //float h = CrossPlatformInputManager.GetAxis("Horizontal");
         //float v = CrossPlatformInputManager.GetAxis("Vertical");
 
-        float h = InputSystemMove.move.x;
-        float v = InputSystemMove.move.y;
-
+        //h = InputSystemMove.move.x;
+        //v = InputSystemMove.move.y;
+        /*
+        Debug_text1.text = h.ToString();
+        Debug_text2.text = v.ToString();
+        */
 #if !MOBILE_INPUT
         float handbrake = CrossPlatformInputManager.GetAxis("Jump");
         m_Car.Move(h, v, v, handbrake);
---
+++ Assets/Scripts/InputSystem/OnButtonText.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnButtonText : MonoBehaviour
+{
+    [SerializeField]
+    Text text3 = null;
+    public void OnButtonText3()
+    {
+        text3.text = "aaaaaaaaaaaaa";
+    }
+    
+}
